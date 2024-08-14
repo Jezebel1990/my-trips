37e800022789f1dca3a4b0b73b87a6b2f863695c
Jezebel1990 <jezebelguedes@gmail.com>
2024-08-14 01:53:48 -0300

    Add map react leaflet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,12 +8,17 @@ import GlobalStyles from 'styles/global'
   return (
      <>
       <Head>
-       <title>React Avançado - Boilerplate</title>
+       <title>My Trips</title>
        <link rel="shortcut icon" href='/img/icon-512.png'/>
        <link rel="apple-touch-icon" href='/img/icon-512.png'/>
        <link rel="manifest" href="/manifest.json"/>
+       <link 
+       rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+     crossOrigin=""
+     />
        <meta name="theme-color" content="#06092B"/>
-       <meta name="description" content='A simple project starter to work with TypeScript, React, NextJS and Styled Components'/>
+       <meta name="description" content='A simple project starter to show my favorite spots in the world.'/>
      </Head>
      <GlobalStyles />
      <Component {...pageProps} />
