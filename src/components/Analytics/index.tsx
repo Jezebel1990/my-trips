2de47d8c4ad1e1a6eaa8427d780e11f8b8ef3a2a
Jezebel Guedes <75287031+Jezebel1990@users.noreply.github.com>
2024-09-06 15:57:52 -0300

    Add  Google Analytics

--- src/components/Analytics/index.tsx
+++ src/components/Analytics/index.tsx
@@ -1,21 +1,24 @@
-import Script from 'next/script'
 import React from 'react'
 
 function Analytics() {
   return (
     <>
-      <Script 
+      <script
+        async
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING}`}
       />
-<Script id="google-analytics">
-  {`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING}');
-  `}
-</Script>
+<script
+   dangerouslySetInnerHTML={{
+      __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING}', {
+               page_path: window.location.pathname,
+         });
+      `
+    }}
+  />
  </>
  )
-}
 export default Analytics
